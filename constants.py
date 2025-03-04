4ed61d77ea10859b3497f33c35cff6469c2e72a1
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-03-04 23:00:07 +0000

    pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Adowk Adiebo
+# Created on: Mar 04, 2025
+# This module contains constants.
+
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.50
